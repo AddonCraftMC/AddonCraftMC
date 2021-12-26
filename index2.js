102cec00189b4463ca1a71084899cee927a096d0
AddonCraftMC <96643815+AddonCraftMC@users.noreply.github.com>
2021-12-26 08:07:14 +0800

    Create index2.js

---
+++ index2.js
@@ -0,0 +1,19 @@
+<script>
+info="<p><strong>Hello User! Welcome to MinecraftFree!!</strong></p>
+<p><em>Features</em>:</p>
+<ul>
+<li>Free Minecraft</li>
+<li>Updates every time Mojang published a new release</li>
+<li>Safe Downloading</li>
+<li>Verified by <a href=&#34;https://virustotal.com&#34;>VirusTotal</a></li>
+</ul>
+<p>Download Latest Release(1.18.2): 
+<a href=&#34;https://mcpedl.org/uploads_files/09-12-2021/minecraft-1-18-2.apk&#34;>Download</a></p>
+<p>Download Latest Beta(1.18.10.24): 
+<a href=&#34;https://mcpedl.org/uploads_files/15-12-2021/minecraft-1-18-10-24.apk&#34;>Download</a></p>
+<p>To Download older versions: <a href=&#34;https://sites.google.com/view/dl-mcpe/&#34;>Click Here</a></p>
+<p><a href=&#34;https://github.com/AddonCraftMC/MinecraftFree&#34;>View On GitHub</a></p>
+<p>Powered by <a href=&#34;https://sites.google.com&#34;>Google Sites</a> and <a href=&#34;https://pages.github.com&#34;>GitHub Pages</a></p>"
+
+document.write(info)
+</script>
